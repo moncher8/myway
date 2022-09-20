4b236788b6771f90319fd7f70faae431ff263f40
moncher8 <kookhotpink@gmail.com>
2022-09-20 21:48:06 +0900

    insert:

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Example"),
+        ),
+        body: Center(
+          child: Text("Helloo Flutter!"),
+        ),
+        bottomNavigationBar: BottomNavigationBar(items: [
+          BottomNavigationBarItem(icon: Icon(Icons.phone), label: "Phone"),
+          BottomNavigationBarItem(icon: Icon(Icons.message), label: "Message"),
+          BottomNavigationBarItem(icon: Icon(Icons.pages), label: "Contact"),
+        ]),
+      ),
+    );
+  }
+}
